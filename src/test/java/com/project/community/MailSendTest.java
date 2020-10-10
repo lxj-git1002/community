946186d3264c2567f363786343b252c7b15b3610
忠厚刘皇叔 <liuxingjun1002@outlook.com>
2020-10-10 21:58:10 +0800

    community 10.10 mail send

--- src/test/java/com/project/community/MailSendTest.java
+++ src/test/java/com/project/community/MailSendTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,9 +18,27 @@ public class MailSendTest {
     @Autowired
     private MailClient mailClient;
 
+    //注入模板引擎
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
     public void test1()
     {
         mailClient.sendMail("lxj10021002@163.com","验证邮件","发送邮件成功！");
+    }
+
+    @Test
+    public void testHtmlEmail()
+    {
+        Context context = new Context();
+        context.setVariable("username","aaa");
+
+        //调用模版引擎生成动态网页
+        String email = templateEngine.process("/mail/demo", context);
+        System.out.println(email);
+
+        //发送邮件
+        mailClient.sendMail("lxj10021002@163.com","验证邮件",email);
     }
 }
